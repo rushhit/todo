7e23fe4e93609be4008f71f4f5ab2492b1ac5b3b
raşit şengün <rasitsengun93@gmail.com>
2020-03-09 23:54:47 +0300

    last

--- js/main.js
+++ js/main.js
@@ -1,34 +1,126 @@
-// function ysYonlendir(ID, adres, saniye) { 
-//     if (saniye == 0) {   
-//         window.location.href = adres;   
-//         return; 
-//     } 
-//     document.getElementById(ID).innerHTML = saniye + " saniye sonra yönlendiriliyorsunuz."; 
-//     saniye--; 
-//     setTimeout(function() {   
-//         ysYonlendir(ID, adres, saniye); 
-//     }, 1000);
-// }
-
 function login() {
-    var username = document.getElementById("inputEU").value;
-    var password = document.getElementById("inputPassword").value;
-    if (username == "Kafein" && password == "Kafein123") {
-
-        swal({
-            title: "Giriş Başarılı!",
-            icon: "success",
-            button: "Kapat",
-        });
-        //document.getElementById("successlogin").innerHTML = "Giriş Başarılı.";
-        // ysYonlendir("uyari", "todolist.html", 5);
-
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == "Kafein" && password == "Kafein") {
+        alert("Giriş Başarılı");
+        window.location = "todolist.html";
+        document.getElementById("username").innerHTML = username;
         return false;
     } else {
-        swal({
-            title: "Giriş Başarısız!",
-            icon: "warning",
-            button: "Kapat",
-        });
+        alert("Kullanıcı adı yada Şifre yanlış");
+        window.location = "index.html";
+        return false;
     }
 }
+
+// todo
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+let LIST, id;
+
+let data = localStorage.getItem("TODO");
+
+
+
+if (data) {
+    LIST = JSON.parse(data);
+    id = LIST.length;
+    loadList(LIST);
+} else {
+    LIST = [];
+    id = 0;
+}
+
+function loadList(array) {
+    array.forEach(function(item) {
+        addToDo(item.name, item.id, item.done, item.trash);
+    });
+}
+
+clear.addEventListener("click", function() {
+    localStorage.clear();
+    location.reload();
+});
+
+const options = { weekday: "long", month: "short", day: "numeric" };
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("tr-TR", options);
+
+
+function addToDo(toDo, id, done, trash) {
+
+    if (trash) { return; }
+
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+
+    const item = `<li class="item">
+                    <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+                    <p class="text ${LINE}">${toDo}</p>
+                    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+                  </li>
+                `;
+
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
+}
+
+document.addEventListener("keyup", function(even) {
+    if (event.keyCode == 13) {
+        const toDo = input.value;
+
+        if (toDo) {
+            addToDo(toDo, id, false, false);
+
+            LIST.push({
+                name: toDo,
+                id: id,
+                done: false,
+                trash: false
+            });
+
+            localStorage.setItem("TODO", JSON.stringify(LIST));
+
+            id++;
+        }
+        input.value = "";
+    }
+});
+
+
+function completeToDo(element) {
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+
+function removeToDo(element) {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+
+    LIST[element.id].trash = true;
+}
+
+
+list.addEventListener("click", function(event) {
+    const element = event.target;
+    const elementJob = element.attributes.job.value;
+
+    if (elementJob == "complete") {
+        completeToDo(element);
+    } else if (elementJob == "delete") {
+        removeToDo(element);
+    }
+
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+});
